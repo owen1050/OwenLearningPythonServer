d4e0c48802dff6ab154c5ff299e700837a6dc2d3
sadmin <sadmin@bserver.fios-router.home>
2019-09-20 15:44:37 +0000

    update

--- server23653.py
+++ server23653.py
@@ -2,16 +2,26 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
 import os
 import time
+import random
 from requests import post
 url = "http://owenserver.us.to:23654"
-
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("content-type", "text/plain")
+        tf = open("diyaWeb","r")
+        web = tf.read()
+        tf.close()
+        self.send_header("content-type", "text/html")
         self.end_headers()
-        self.wfile.write("Im always thinking about you".encode())
+        r1 = random.randint(0,0)
+        if r1 == 0:
+            #self.wfile.write("Im always thinking about you".encode())
+            self.wfile.write(web.encode())
+        if r1 == 1:
+            self.wfile.write("You look beautiful today Diya<3".encode())
+        if r1 == 2:
+            self.wfile.write("I cant wait until you are in my arms again".encode())
         com = "set:hallLightOn=1;"
         post(url = url, data = {"content":com})
         
