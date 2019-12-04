c56b10765d9a5b8a1ca8661817035fbde0223905
sadmin <sadmin@bserver.fios-router.home>
2019-12-04 16:41:47 +0000

    start work on email recipt

--- server23653.py
+++ server23653.py
@@ -10,15 +10,24 @@ url = "http://owenserver.us.to:23654"
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         url = "https://maker.ifttt.com/trigger/monitorip/with/key/Bf91G_MsjKUzsWqRs5N7n"
-        get(url=url, params = dict(value1 = "FORTNITE"))        
+        #get(url=url, params = dict(value1 = "FORTNITE"))        
         
         self.send_response(200)
         tf = open("diyaWeb","r")
         web = tf.read()
         tf.close()
-        self.send_header("content-type", "text/html")
+
+        pic = open("picture.png", "rb")
+        picture = pic.read()
+        pic.close()
+        info = os.stat("picture.png")
+        size = info.st_size
+
+        self.send_header("Content-type", "image/jpg")
+        self.send_header("Content-length", size)
         self.end_headers()
         r1 = random.randint(0,0)
+        r1 = 3
         if r1 == 0:
             #self.wfile.write("Im always thinking about you".encode())
             self.wfile.write(web.encode())
@@ -26,6 +35,9 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             self.wfile.write("You look beautiful today Diya<3".encode())
         if r1 == 2:
             self.wfile.write("I cant wait until you are in my arms again".encode())
+        if r1 == 3:
+            self.wfile.write(picture)
+        print(self.path)
         com = "set:hallLightOn=1;"
        # post(url = url, data = {"content":com})
         
